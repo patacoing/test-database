b49887d09a1cd66fd4d8c344a95400d983167476
patacoing <romain.lefebvre1104@gmail.com>
2023-09-22 00:04:44 +0200

    [ADD] args to add schemas

--- api/main.py
+++ api/main.py
@@ -4,6 +4,8 @@ from app.settings import settings
 from app.tools.engine_sqlalchemy import Base, engine_mysql, engine_postgresql
 from app.sqlalchemy_models.Todo import Todo
 
+import argparse
+
 from app.routers.mysql import router as mysql_router
 from app.routers.postgresql import router as postgresql_router
 # If we want to auto build the tables we have to import the table models and then run the Base.metadat.create_all function
@@ -14,6 +16,12 @@ app.include_router(mysql_router, prefix="/mysql")
 app.include_router(postgresql_router, prefix="/postgresql")
 
 if __name__ == "__main__":
-    print(settings)
-    # Base.metadata.create_all(bind=engine_mysql)
-    Base.metadata.create_all(bind=engine_postgresql)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--build-mysql", action="store_true")
+    parser.add_argument("--build-postgresql", action="store_true")
+
+    args = parser.parse_args()
+    if args.build_mysql:
+        Base.metadata.create_all(bind=engine_mysql)
+    if args.build_postgresql:
+        Base.metadata.create_all(bind=engine_postgresql)
