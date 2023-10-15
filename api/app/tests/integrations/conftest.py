a7ceea02c0d5b48ecb4c4cb8db12889196e88130
Romain Lefebvre <romain.lefebvre1104@gmail.com>
2023-10-15 12:49:54 +0200

    [UPDATE]

--- api/app/tests/integrations/conftest.py
+++ api/app/tests/integrations/conftest.py
@@ -10,6 +10,7 @@ from app.tools.engine_sqlalchemy import Base, get_db_mysql, get_db_postgresql
 from app.tools.redis import Redis
 from app.tools.redis import get_redis_client
 
+print(f"mysql+mysqlconnector://{settings.MYSQL_TEST_USER}:{settings.MYSQL_TEST_PASSWORD}@{settings.MYSQL_TEST_HOST}:{settings.MYSQL_TEST_PORT}/{settings.MYSQL_TEST_DATABASE}")
 
 engine_mysql = create_engine(
     f"mysql+mysqlconnector://{settings.MYSQL_TEST_USER}:{settings.MYSQL_TEST_PASSWORD}@{settings.MYSQL_TEST_HOST}:{settings.MYSQL_TEST_PORT}/{settings.MYSQL_TEST_DATABASE}",
@@ -29,6 +30,7 @@ redis_client_db = Redis(
     port=settings.REDIS_TEST_PORT,
     decode_responses=True
 )
+
 
 def override_get_db_mysql():
     try:
